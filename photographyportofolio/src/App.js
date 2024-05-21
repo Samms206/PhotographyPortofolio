0daeb5adb97be0b79641229ca7815f9d89cca45d
Samms206 <sh2069936@gmail.com>
2024-05-21 23:00:25 +0700

    updated

---
+++ photographyportofolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+import AnimRoutes from './components/AnimRoutes';
+
+function App() {
+  return (
+    <div>
+      <AnimRoutes>
+        
+      </AnimRoutes>
+    </div>
+  );
+}
+
+export default App;
---
+++ photographyportofolio/src/components/AnimRoutes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AnimRoutes = () => {
+  return (
+    <div>
+      Anime Routes
+    </div>
+  )
+}
+
+export default AnimRoutes
+
