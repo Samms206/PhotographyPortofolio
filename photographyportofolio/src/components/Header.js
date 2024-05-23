a8b0999449951228b58aae37a3a9bc7891b8fde6
Samms206 <sh2069936@gmail.com>
2024-05-23 23:12:55 +0700

    updated

--- photographyportofolio/src/components/Header.js
+++ photographyportofolio/src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='header'>
-      
+    <div className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+      Header
     </div>
   )
 }
