a9a65d026d6c659ad8a6c20c20a327e8828339da
Samms206 <sh2069936@gmail.com>
2024-05-24 21:30:21 +0700

    updated

--- photographyportofolio/src/components/Header.js
+++ photographyportofolio/src/components/Header.js
@@ -6,8 +6,12 @@ const Header = () => {
     <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
       <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
         <Link to={"/"}>
-        <img src='./img/header/logo.svg' alt='logo'/>
+          <img src='./img/header/logo.svg' alt='logo'/>
         </Link>
+
+        <nav className='hidden lg:flex'>
+          <Link to={'/'} className='text-[#696c6d] hover:text-primary transition'/>
+        </nav>
       </div>
     </header>
   )
