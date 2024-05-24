7a4b2b2185ee7d478b37c08d1dea21bd112f1143
Samms206 <sh2069936@gmail.com>
2024-05-24 21:28:10 +0700

    updated

--- photographyportofolio/src/components/Header.js
+++ photographyportofolio/src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
-      Header
-    </div>
+    <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+      <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
+        <Link to={"/"}>
+        <img src='./img/header/logo.svg' alt='logo'/>
+        </Link>
+      </div>
+    </header>
   )
 }
 
